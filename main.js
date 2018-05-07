474c2918cdc90553af086997f396c1c4ebefd2b2
Jeff Bender <j25bender@gmail.com>
2018-05-07 16:10:10 -0600

    Completed Lesson 7

--- main.js
+++ main.js
@@ -5,19 +5,43 @@
 const app = new Vue({ 
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: 'assets/green-socks.jpeg',
+    selectedVariant: 0,
     inventory: 0,
     details: ['80% Cotton', '20% Polyester', 'Gender Neutral'],
     variants: [
       {
         variantColor: 'blue',
-        variantId: 1
+        variantId: 1,
+        variantImage: 'assets/blue-socks.jpeg',
+        variantQuantity: 0
       },
       {
         variantColor: 'green',
-        variantId: 2
+        variantId: 2,
+        variantImage: 'assets/green-socks.jpeg',
+        variantQuantity: 10
       }
-    ]
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() { this.cart += 1 },
+    updateProduct(index) { this.selectedVariant = index }
+  },
+  // Computed properties only run when dependencies change / update
+  // Values are cached / saved until updated
+  // brand and product are dependencies of title
+  computed: {
+    title() { 
+      return this.brand + ' ' + this.product
+    },
+    image() { 
+      return this.variants[ this.selectedVariant ].variantImage
+    },
+    inStock() { 
+      return this.variants[ this.selectedVariant ].variantQuantity
+    }
   }
 })
